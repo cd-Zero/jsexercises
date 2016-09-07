3fdd2349ef665967bda769aabcc230611acf7ac1
zero <mike.aduayi>
2016-09-07 11:58:35 -0400

    solves for solution
    solves for solution and has comments

--- jsExercises/problem1.js
+++ jsExercises/problem1.js
@@ -1,9 +1,12 @@
 console.log('test')
-
-
+// variable that will hold our value of i
+var total = 0;
+// for loop since we know end condition. iterates to number 1000 by 1 int. 
 for (var i = 0; i <= 1000; i++) {
 	if ( i % 3 === 0|| i % 5 === 0) {
-
-		console.log(i)
+        // stores and consecutively adds value of i to total through each iteration
+		console.log(total += i)
 	}
 }
+//'total' value will appear via alert box. sum of every int between 1000 evenly devisible by 3 OR 5 will show. 
+alert(total);
